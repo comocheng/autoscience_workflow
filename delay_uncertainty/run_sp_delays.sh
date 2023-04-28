abea1bc1eb42be41ad160f8d40d250b80a99eb61
Sevy Harris <harris.se@northeastern.edu>
2023-04-28 08:39:34 -0400

    add code for compiling delay matrix

---
+++ delay_uncertainty/run_sp_delays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=delay1
+#SBATCH --mem=20Gb
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=26
+#SBATCH --array=1-12
+
+
+python /work/westgroup/harris.se/autoscience/reaction_calculator/delay_uncertainty/make_sp_delay_npys.py $1 $SLURM_ARRAY_TASK_ID
