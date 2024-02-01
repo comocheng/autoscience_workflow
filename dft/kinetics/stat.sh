05f3b7a172b1c17f2c026532b0541d068ea8e121
Sevy Harris <harris.se@northeastern.edu>
2024-02-01 10:13:14 -0500

    added quick check of reaction status

---
+++ dft/kinetics/stat.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+suffix=$(printf "%06.0f" $(($1)))
+reaction_dir="/work/westgroup/harris.se/autoscience/reaction_calculator/dft/kinetics/reaction_$suffix"
+
+if test -f "$reaction_dir/arkane/RMG_libraries/reactions.py" ; then
+    echo "Arkane complete"
+    exit 0
+else
+    echo "Arkane:"
+    if test -d "$reaction_dir/arkane/" ; then
+        ls
+    fi
+fi
+echo
+
+echo "Overall:"
+if test -d "$reaction_dir/overall/" ; then
+    cd "$reaction_dir/overall/"
+    grep -l Normal *_*.log
+fi
+echo
+
+echo "Center:"
+if test -d "$reaction_dir/center/" ; then
+    cd "$reaction_dir/center/"
+    grep -l Normal *_*.log
+fi
+echo
+
+echo "Shell:"
+if test -d "$reaction_dir/shell/" ; then
+    cd "$reaction_dir/shell/"
+    grep -l Normal *_*.log
+fi
+echo
+
+echo "HFSP:"
+if test -d "$reaction_dir/hfsp/" ; then
+    cd "$reaction_dir/hfsp/"
+    grep -l Normal *_*.log
+fi
+echo
