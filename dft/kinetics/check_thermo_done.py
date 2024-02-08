714fe4848ff5b94cb77319542421f82e0a021b83
Sevy Harris <harris.se@northeastern.edu>
2024-02-08 12:59:13 -0500

    add script to check if species for reaction are missing

---
+++ dft/kinetics/check_thermo_done.py
@@ -0,0 +1,25 @@
+import sys
+import pandas as pd
+import os
+
+reaction_index = int(sys.argv[1])
+
+
+df = pd.read_csv('/work/westgroup/harris.se/autoscience/reaction_calculator/dft/reaction_database.csv')
+#print(df)
+
+unique_string = df['unique_string'].values[reaction_index]
+
+reactants = [int(x) for x in unique_string.split('=')[0].split('+')]
+products = [int(x) for x in unique_string.split('=')[1].split('+')]
+
+incomplete = False
+for species_index in reactants + products:
+    if not os.path.exists(os.path.join(f'/work/westgroup/harris.se/autoscience/reaction_calculator/dft/thermo/species_{species_index:04}/arkane/RMG_libraries/thermo.py')):
+        print(species_index, 'is not done')
+
+        incomplete = True
+
+if not incomplete:
+    print('All species are done', unique_string)
+
