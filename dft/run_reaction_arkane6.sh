25599d72012be16a226c3febaf4fd5769b4d2891
Sevy Harris <harris.se@northeastern.edu>
2024-04-11 10:10:45 -0400

    removing 6 from naming scheme now that 6 digits is default

--- dft/run_reaction_arkane6.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#SBATCH --time=01:00:00
-
-python /work/westgroup/harris.se/autoscience/reaction_calculator/dft/run_reaction_arkane6.py $1 $2
-# python-jl /work/westgroup/harris.se/autoscience/reaction_calculator/dft/run_reaction_arkane6.py $1 $2
