4b31716bca1cd1e937395e3aed282730d1bd3d62
Sevy Harris <harris.se@northeastern.edu>
2024-05-15 12:26:54 -0400

    deleting obsolete flame speed scripts

--- plot_flame_speeds/run_flames.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --ntasks=26
-#SBATCH --constraint=cascadelake
-#SBATCH --time=24:00:00
-
-python /work/westgroup/harris.se/autoscience/reaction_calculator/plot_flame_speeds/run_flame_speeds.py $1
