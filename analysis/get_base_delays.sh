221f77875e12d98a6110c4a9202443cad7b44f1d
Sevy Harris <harris.se@northeastern.edu>
2024-09-19 08:52:42 -0400

    only compute base delays for table 7

--- analysis/get_base_delays.sh
+++ analysis/get_base_delays.sh
@@ -3,6 +3,6 @@
 #SBATCH --mem=20Gb
 #SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=26
-#SBATCH --array=1-12
+#SBATCH --array=7
 
 python $AUTOSCIENCE_REPO/analysis/get_base_delays.py $1 $SLURM_ARRAY_TASK_ID
