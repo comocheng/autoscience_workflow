966fddf7788e91b84d2275872556c9b9a70e0687
Sevy Harris <harris.se@northeastern.edu>
2024-09-19 10:38:11 -0400

    move code updating harris_butane DFT uncertainty to export uncertainty script

--- analysis/export_uncertainty.py
+++ analysis/export_uncertainty.py
@@ -28,6 +28,17 @@ kinetic_libs = [  # <------------------------- edit these to match input file li
 ]
 # ----------------------------------------------------------------------------
 
+DFT_THERMO_UNCERTAINTY = 3.0
+DFT_KINETIC_UNCERTAINTY = 1 / np.sqrt(3) * np.log(10)
+calculated_thermo_libs = [
+    'harris_butane'  # <----- edit these to match name of thermo library calculated during this workflow
+]
+
+calculated_kinetic_libs = [
+    'harris_butane'  # <----- edit these to match names of kinetics libraries calculated during this workflow
+]
+
+
 uncertainty = rmgpy.tools.uncertainty.Uncertainty(output_directory=os.path.join(mech_dir, 'rmg_uncertainty'))
 uncertainty.load_model(chemkin, species_dict)
 
@@ -41,6 +52,22 @@ uncertainty.load_database(
 # Get the different kinetic and thermo sources
 uncertainty.extract_sources_from_model()
 uncertainty.assign_parameter_uncertainties()
+
+
+# change values of parameters calculated by this workflow
+# RMG automatically estimates uncertainty as 0.5 for kinetics libraries
+# but should be closer to ~1.1 for this workflow
+for i in range(len(uncertainty.species_list)):
+    if 'Thermo library: ' in uncertainty.species_list[i].thermo.comment:
+        thermo_library = uncertainty.species_list[i].thermo.comment[len('Thermo library: '):]
+        if thermo_library.lower() in calculated_thermo_libs:
+            uncertainty.thermo_input_uncertainties[i] = DFT_THERMO_UNCERTAINTY
+
+for i in range(len(uncertainty.species_list)):
+    if hasattr(uncertainty.reaction_list[i], 'library'):
+        if uncertainty.reaction_list[i].library.lower() in calculated_kinetic_libs:
+            uncertainty.kinetic_input_uncertainties[i] = DFT_KINETIC_UNCERTAINTY
+
 
 np.save(os.path.join(mech_dir, 'gao_reaction_uncertainty.npy'), uncertainty.kinetic_input_uncertainties)
 np.save(os.path.join(mech_dir, 'gao_species_uncertainty.npy'), uncertainty.thermo_input_uncertainties)
