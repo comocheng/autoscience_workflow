110b6fe6c92f3c1e32d3643fb532d0805ce0c515
Sevy Harris <harris.se@northeastern.edu>
2025-05-12 12:56:23 -0400

    fix module loading for expanse ts rotors

--- dft/slurm_scripts/relaxed_ts_rotors_expanse.sh
+++ dft/slurm_scripts/relaxed_ts_rotors_expanse.sh
@@ -9,11 +9,19 @@
 #SBATCH --nodes=1
 #SBATCH --array={array}
 
-module load gaussian/g16
-source /shared/centos7/gaussian/g16/bsd/g16.profile
 
-cd {rotor_dir}
+module reset
+module unload cpu/0.17.3b
+module load cpu/0.15.4
+module load gaussian/16.C.01
+exe=`which g16`
+export GAUSS_SCRDIR=/scratch/$USER/job_$SLURM_JOBID
+
 RUN_i=$(printf "%04.0f" $(($SLURM_ARRAY_TASK_ID)))
-fname="rotor_$RUN_i.com"
+in_fname="rotor_$RUN_i.com"
+out_fname="rotor_$RUN_i.log"
 
-g16 $fname
+bash /cm/shared/examples/sdsc/gaussian/cpu/getcpusets $$
+cat $$.out $in_fname >file.tmp.$$
+/usr/bin/time $exe < file.tmp.$$ > $out_fname
+rm -f $$.out file.tmp.$$
