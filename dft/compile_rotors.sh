694b099aa23fd3ed95ab3b894c45dff4b7db3323
Sevy Harris <sharris@exp-9-55.expanse.sdsc.edu>
2025-05-13 06:43:24 -0700

    add script for expanse compiling ts rotors

--- dft/compile_rotors.sh
+++ dft/compile_rotors.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#SBATCH --partition=express,short,west
-#SBATCH --time=00:00:04
+#SBATCH --partition=preempt
+#SBATCH --time=00:00:10
+#SBATCH --account=nrt112
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --export=ALL
 
 python $AUTOSCIENCE_REPO/dft/compile_rotors.py $1 $2
 
