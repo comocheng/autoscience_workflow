b407159ef89c751e45144e91b22676ade8b3fd8d
Sevy Harris <sharris@exp-9-55.expanse.sdsc.edu>
2025-05-13 06:33:55 -0700

    add expanse reaction arkane script params

--- dft/run_reaction_arkane.sh
+++ dft/run_reaction_arkane.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #SBATCH --time=01:00:00
+#SBATCH --partition=preempt
+#SBATCH --account=nrt112
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --export=ALL
+
 
 python $AUTOSCIENCE_REPO/dft/run_reaction_arkane.py $1 $2
 # python-jl $AUTOSCIENCE_REPO/dft/run_reaction_arkane.py $1 $2
