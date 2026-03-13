75084740651cb1bbcd614111c0b2289abbca1019
Sevy Harris <harris.se@northeastern.edu>
2026-03-13 11:21:50 -0400

    remove DIB scripts for refactor

--- analysis/dib_get_base_delays.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=base_delays
-#SBATCH --mem=20Gb
-#SBATCH --time=02:00:00
-#SBATCH --cpus-per-task=26
-#SBATCH --array=24
-
-python $AUTOSCIENCE_REPO/analysis/dib_get_base_delays.py $1 $SLURM_ARRAY_TASK_ID
--- analysis/dib_get_delays_at_experiment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=base_delays
-#SBATCH --mem=20Gb
-#SBATCH --time=02:00:00
-#SBATCH --cpus-per-task=26
-
-python $AUTOSCIENCE_REPO/analysis/dib_get_delays_at_experiment.py $1
